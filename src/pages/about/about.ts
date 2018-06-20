798f3cb18e33c29d20d85658421b06931ad0e51f
virtohoho <vitor.silva615@etec.sp.gov.br>
2018-06-20 20:57:23 -0300

    Ultima previa

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -14,88 +14,33 @@ declare var google;
 export class AboutPage {
 
   linha: any [];
-  directionsService = new google.maps.DirectionsService();
-  directionsDisplay = new google.maps.DirectionsRenderer();
-  map: any;
+  //directionsService = new google.maps.DirectionsService();
+  //directionsDisplay = new google.maps.DirectionsRenderer();
+  //map: any;
   //inicia o mapa na ETEC
-  startPosition = new google.maps.LatLng(-23.552994,-46.399617);
-  originPosition: string;
-  destinationPosition: string;
-  localAtual: string;
+  //startPosition = new google.maps.LatLng(-23.552994,-46.399617);
+  //originPosition: string;
+  //destinationPosition: string;
+  //localAtual: string;
 
-  
+  originPosition: any[];
   ionViewDidLoad() {
     
   }
 
   constructor(public navCtrl: NavController ,private geolocation: Geolocation,public alertCtrl: AlertController,public service: ServiceProvider) {
-    this.initializeMap();
     this.getLinha();
   }
-  pegarLocalizacao(){
-    this.geolocation.getCurrentPosition()
-    .then((resp) => {
-      this.originPosition = new google.maps.LatLng(resp.coords.latitude, resp.coords.longitude);
-      //troca a posiinicial inicia do mapa para a localização atual
-      this.startPosition = this.originPosition;
-      
-      this.initializeMap();
 
-    }).catch((error) => {
-      console.log('Erro ao recuperar sua posição', error);
-    });
-  }
-  
-  initializeMap() {
-    const mapOptions = {
-      zoom: 17,
-      center: this.startPosition,
-      disableDefaultUI: true
-    }
-
-    this.map = new google.maps.Map(document.getElementById('map'), mapOptions);
-    this.directionsDisplay.setMap(this.map);
-
-    const marker = new google.maps.Marker({
-      position: this.startPosition,
-      map: this.map,
-    });
-  }
 
   getLinha(){
-    this.service.getLinha().subscribe(
-      data=> this.linha=data,
+    this.service.getInfo().subscribe(
+      data=> console.log(this.linha=data),
       err=>console.log(err)
     )
-
-    
   }
-
-  calculateRoute() {
-    if (this.destinationPosition && this.originPosition) {
-      const request = {
-        // Pode ser uma coordenada (LatLng), uma string ou um lugar
-        origin: this.originPosition,
-        destination: this.destinationPosition,
-        travelMode: 'TRANSIT',
-        transitOptions: {
-          modes: ['BUS'],
-          trafficModel: 'best_guess' 
-        },
-      } ;
-      this.traceRoute(this.directionsService, this.directionsDisplay, request);
-    }
-  }
-
-  traceRoute(service: any, display: any, request: any) {
-    service.route(request, function (result, status) {
-      if (status == 'OK') {
-        display.setDirections(result);
-      }
-    });
-  }
-
-  showRadio(num,codigo){
+  
+  showRadio(originPosition){
 
 
    
@@ -103,16 +48,16 @@ export class AboutPage {
     alert.setTitle('Sentido');
       alert.addInput({
         type: 'radio',
-        label: this.linha[num].descricaoPonto,
+        label: this.linha[originPosition].descricaoPonto,
         name:'Ida',
-        value: this.linha[num].codPonto,
+        value: this.linha[originPosition].codPonto,
         checked: true
       });
       alert.addInput({
         type: 'radio',
         name:'volta',
-        label: this.linha[num-1].descricaoPonto,
-         value: this.linha[num-1].codPonto,
+        label: this.linha[originPosition-1].descricaoPonto,
+         value: this.linha[originPosition-1].codPonto,
         checked: false
       });
 
@@ -122,9 +67,10 @@ export class AboutPage {
       text: 'OK',
       handler: data => {
         if(alert.data.inputs[0].checked == false){
-          this.postDados(this.linha[num-1].codPonto,codigo);
+        (this.linha[originPosition-1].codPonto);
         }else if(alert.data.inputs[0].checked == true){
-          this.postDados(this.linha[num].codPonto,codigo);
+          (this.linha[originPosition].codPonto);
+          console.log(this.linha)
         }
           
     //    this.testRadioOpen = false;
@@ -144,10 +90,4 @@ export class AboutPage {
       }
 
 
-
-  private newMethod() {
-    postDados(this.destinationPosition);
-    {
-    }
-  }
 }
